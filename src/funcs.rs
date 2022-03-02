ed2639cee57fb2fbb449cf85729a54689a9636d2
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2022-03-02 08:24:58 -0600

    simplify code

--- src/funcs.rs
+++ src/funcs.rs
@@ -1,52 +1,28 @@
 extern crate reqwest;
-extern crate indicatif;
-extern crate thread_control;
 extern crate zip_extract;
 use zip_extract::*;
 use reqwest::*;
-use indicatif::*;
-use thread_control::*;
 use std::path::*;
-use std::fs::*;
 use std::*;
+use std::io::*;
 
-pub fn download(arg: &String, last: bool) -> String {
+pub fn download(arg: &String) -> (String, Cursor<Vec<u8>>) {
     let mut req = blocking::get(arg).unwrap();
     let num = arg.rfind("/").unwrap();
     let name = &arg[num + 1..];
-    let mut out = File::create(name).unwrap();
-    let prog = ProgressBar::new(req.content_length().unwrap());
-    prog.set_style(ProgressStyle::default_bar()
-    .template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes}")
-    .progress_chars("#>-"));
-    let (flag, control) = make_pair();
-    let handel = thread::spawn(move || {
-        while flag.is_alive() {
-            io::copy(&mut req, &mut out).unwrap();
-            break;
-        }
-    });
-    while !control.is_done() {
-        prog.set_position(fs::metadata(name).unwrap().len());
-        if control.is_done() {
-            control.stop();
-            break;
-        }
-    }
-    handel.join().unwrap();
-    prog.set_position(prog.length());
-    if last {
-        prog.finish();
-    }
-    return String::from(name);
+    let mut out: Cursor<Vec<u8>> = Cursor::new(Vec::new());
+    io::copy(&mut req, &mut out).unwrap();
+    return (String::from(name), out);
 }
 
-pub fn downloadandunzip(arg: &String, last: bool) {
-    let name = &download(arg, last);
-    let folder = Path::new(Path::new(name).file_stem().unwrap().to_str().unwrap());
-    let file = File::open(name).unwrap();
+pub fn downloadandunzip(arg: &String) {
+    let num = arg.rfind("/").unwrap();
+    let name = &arg[num + 1..];
+    println!("Downloading {}", name);
+    let pair = download(arg);
+    let (name, file) = pair;
+    let folder = Path::new(Path::new(&name).file_stem().unwrap());
     extract(file, folder, false).unwrap();
-    remove_file(name).unwrap();
 }
 
 pub fn createstr(arg: &str) -> String {
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod funcs;
 use std::*;
 
 fn main() {
-    funcs::downloadandunzip(&funcs::createstr("https://cdn.discordapp.com/attachments/886616711925751829/948264229226565662/CodeWarrior.zip"), false);
-    funcs::downloadandunzip(&funcs::createstr("https://cdn.discordapp.com/attachments/886616711925751829/948264229817958420/Kamek.zip"), true);
+    funcs::downloadandunzip(&funcs::createstr("https://cdn.discordapp.com/attachments/886616711925751829/948264229226565662/CodeWarrior.zip"));
+    funcs::downloadandunzip(&funcs::createstr("https://cdn.discordapp.com/attachments/886616711925751829/948264229817958420/Kamek.zip"));
     println!("Downloaded!");
 }
