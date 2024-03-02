e1e35162a2d358c743564a8ac014ecd7faab7a3c
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2024-03-02 11:38:33 -0600

    new update: we are relying on new links for this.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 mod funcs;
 
 use std::io::*;
+use std::fs;
+use std::path::Path;
 
 static LINKS: &[u8] = include_bytes!("..\\links.txt");
 
 fn main() {
+    if Path::new("CodeWarrior").exists() {
+        fs::remove_dir_all("CodeWarrior").unwrap();
+    }
+    if Path::new("Kamek").exists() {
+        fs::remove_dir_all("Kamek").unwrap();
+    }
     let links = &getlinks();
     for link in links {
         funcs::downloadandunzip(link);
     }
+    fs::rename("CodeWarrior-Syati", "CodeWarrior").unwrap();
+    fs::rename("Kamek-v1", "Kamek").unwrap();
     println!("Downloaded!")
 }
 
@@ -19,4 +29,11 @@ fn getlinks() -> Vec<String> {
         res.push(line.unwrap());
     }
     res
+}
+
+#[test]
+fn test() -> std::result::Result<(), Box<dyn std::error::Error>> {
+    fs::rename("CodeWarrior-Syati", "CodeWarrior")?;
+    fs::rename("Kamek-v1", "Kamek")?;
+    Ok(())
 }
